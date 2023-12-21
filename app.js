8dd2cf852066c9e7f1ffcdfa6ce713e1d0d4ff93
abijeet-dhillon <abijeetdhillon@gmail.com>
2023-12-20 18:57:36 -0800

    Pseudo implemented the home page with transitions. Still need to fill in information properly and add a headshot/picture of self.

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ function Transition() {
             this.className += ' active-btn';
         })
     }
+    allSections.addEventListener('click', (e) => {
+        console.log("hi");
+        const id = e.target.getAttribute('data-id');
+        console.log(id);
+
+        if(id) {
+            sectionControls.forEach((control) => {
+                control.classList.remove('active')
+            })
+            e.target.classList.add('active')  
+            individualSections.forEach((section) => {
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 // Call function to handle transitions between sections
 Transition();
